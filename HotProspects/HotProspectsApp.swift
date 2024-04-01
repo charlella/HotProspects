c578375fdc0aef5903629f1405d7f1af726b5c52
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-04-01 18:01:13 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by charlene hoareau on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
