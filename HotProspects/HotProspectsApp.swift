fab553654ec846589c66112787c7cbb001ea8564
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-04-04 22:07:08 +0200

    feat: Add custom tabBar, SwiftData, QRcode, swipeActions

--- HotProspects/HotProspectsApp.swift
+++ HotProspects/HotProspectsApp.swift
@@ -5,6 +5,7 @@
 //  Created by charlene hoareau on 01/04/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct HotProspectsApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Prospect.self)
     }
 }
